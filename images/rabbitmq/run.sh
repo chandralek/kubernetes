9794e0314378611131754302567b82bb0583a343
Chandralekha.s <chandralekha.s@sophos.com>
2020-06-04 06:37:04 +0530

    Updating kubernetes module

--- images/rabbitmq/run.sh
+++ images/rabbitmq/run.sh
@@ -2,11 +2,6 @@
 
 set -x
 
-yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-yum install rabbitmq-server
-systemctl start rabbitmq-server
-
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_user_tags roboshop administrator
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
